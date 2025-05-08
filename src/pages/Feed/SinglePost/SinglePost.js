9c91aa9cf8071f57be384c76705b569b1bb6e764
Nu 's <nus@Nus-MacBook-Air.local>
2025-05-08 16:59:37 +0700

    update image

--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -29,7 +29,7 @@ class SinglePost extends Component {
         this.setState({
           title: resData.post.title,
           author: resData.post.creator.name,
-          image: 'http://localhost:8080/' + resData.post.imageUrl,
+          image: `${process.env.REACT_APP_API_URL}` + resData.post.imageUrl,
           date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
           content: resData.post.content
         });
