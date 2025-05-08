05b87117efbc9c15fe86a86e4d159d33b7bbaa33
Nu 's <nus@Nus-MacBook-Air.local>
2025-05-08 16:28:37 +0700

    Update API URL to point to Render backend

--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -14,7 +14,7 @@ class SinglePost extends Component {
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    fetch('http://localhost:8080/feed/post/' + postId, {
+    fetch(`${process.env.REACT_APP_API_URL}/feed/post/` + postId, {
       headers: {
         Authorization: 'Bearer ' + this.props.token
       }
